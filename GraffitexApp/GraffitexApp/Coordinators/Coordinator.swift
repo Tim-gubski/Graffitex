e82ff8952e57e10e1933c2f6d1ad00ac532c579e
Nihal Syed <syednihal4@gmail.com>
2022-09-17 11:30:00 -0400

    New File strucutre added
    Added Coordinator class and extra folders for structure

---
+++ GraffitexApp/GraffitexApp/Coordinators/Coordinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinator.swift
+//  GraffitexApp
+//
+//  Created by Nihal Syed on 2022-09-17.
+//
+
+import Foundation
+import RealityKit
+import SwiftUI
+
+class Coordinator{
+    var arView: ARView?
+    
+}
