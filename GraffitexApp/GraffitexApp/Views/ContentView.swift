2a10b3d0b6cf5aa1fd0594d4d5c6400b14d1e0a8
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2022-09-18 02:56:39 -0400

    shaking sound

--- GraffitexApp/GraffitexApp/Views/ContentView.swift
+++ GraffitexApp/GraffitexApp/Views/ContentView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import CoreMotion
 
 struct ContentView : View {
+    @State private var audioPlayer: AVAudioPlayer?
+    @State private var y1: Double = 0.001
+    @State private var y2: Double = 0.001
+    @State private var y3: Double = 0.001
+    @State private var delta1: Double = 0.0
+    @State private var delta2: Double = 0.0
+    @State private var sign1: Int = 0
+    @State private var sign2: Int = 0
+    
     var body: some View {
         return ZStack {
             ARViewContainer().edgesIgnoringSafeArea(.all)
@@ -19,12 +29,78 @@ struct ContentView : View {
                 ButtonBarUpper()
                 ButtonBarLower()
                 
+            }.onAppear{
+                // Create a CMMotionManager instance
+                let manager = CMMotionManager()
+
+                // Read the most recent accelerometer value
+
+                // How frequently to read accelerometer updates, in seconds
+                manager.accelerometerUpdateInterval = 0.08
+
+                // Start accelerometer updates on a specific thread
+                manager.startAccelerometerUpdates(to: .main) { (data, error) in
+                    if let accelerometerData = manager.accelerometerData {
+                        print(accelerometerData.acceleration.x,
+                              accelerometerData.acceleration.y,
+                              accelerometerData.acceleration.z)
+//                        if(abs(accelerometerData.acceleration.x)>1.5 || abs(accelerometerData.acceleration.y)>1.5 ||
+//                           abs(accelerometerData.acceleration.z)>1.5){
+//                            print("MAKE NOISE")
+//                            startAudio()
+//                        }
+                        do{
+                            y1 = y2
+                            y2 = y3
+                            y3 = accelerometerData.acceleration.y
+                            delta1 = y2-y1
+                            delta2 = y3-y2
+                            sign1 = Int(delta1/abs(delta1+1e-14))
+                            sign2 = Int(delta2/abs(delta2+1e-14))
+                            if ((min(abs(delta1),abs(delta2)) > 2) && (sign1 != sign2)) {
+                                startAudio()
+                            }
+                        }catch{
+                            print("error \(error.localizedDescription)")
+                        }
+                    }
+                }
             }
         }
         .edgesIgnoringSafeArea(.all)
 //
     }
+    
+    func startAudio(){
+        // Load a local sound file
+        guard let soundFileURL = Bundle.main.url(
+            forResource: "ballSound",
+            withExtension: "mp3"
+        ) else {
+            return
+        }
+        
+        do {
+            // Configure and activate the AVAudioSession
+            try AVAudioSession.sharedInstance().setCategory(
+                AVAudioSession.Category.playback
+            )
+
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            // Play a sound
+            audioPlayer = try AVAudioPlayer(
+                contentsOf: soundFileURL
+            )
+            audioPlayer?.numberOfLoops = 1
+            audioPlayer?.play()
+        }
+        catch {
+            print("error \(error.localizedDescription)")
+        }
+    }
 }
+
 
 struct ARViewContainer: UIViewRepresentable {
     @State public var arView : ARView?
